fc5b7b9be0f351b526681ee60c1ad66ba2b313f0
Blake Tan <blaketan@Blakes-MacBook-Pro-2.local>
2015-04-24 15:45:32 -0500

    included time offset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   	def rm_avail(room)
+		offset = 60*60*5
   		t = Time.now
-  		rmevents = Event.today.where(room_id: room).load
+  		rmevents = Event.where(room_id: room).today.load
+  		puts "Room events today : #{rmevents.count}"
 		available = true
 		next_timing = Time.new(t.year,t.month,t.day,22)
 		if t.hour < 7 
 			available = false
 			next_timing = Time.new(t.year,t.month,t.day,7)
-		elsif t.hour >22
-			next_timing = Time.new(t.year,t.month,t.day,7)
+		elsif t.hour >21
+			available = false
+			next_timing = Time.new(t.year,t.month,t.day,7)	
 		else
-			for i in 0..(rmevents.count-1)
-				if t >= rmevents[i][:start_time].to_time and t < rmevents[i][:end_time].to_time
+			for i in 0..(rmevents.count - 1)
+				if t >= (rmevents[i][:start_time].to_time+offset) and t < (rmevents[i][:end_time].to_time+offset)
 					available = false
-					next_timing = rmevents[i][:end_time].to_time
+					next_timing = (rmevents[i][:end_time].to_time+offset)
 					break
-				elsif t < rmevents[i][:start_time].to_time and t > rmevents[i-1][:end_time].to_time
-					next_timing = rmevents[i][:start_time].to_time
+				elsif t < (rmevents[i][:start_time].to_time+offset) and t > (rmevents[i-1][:end_time].to_time+offset)
+					next_timing = (rmevents[i][:start_time].to_time+offset)
 					break
 				end
 			end
@@ -31,7 +34,4 @@ class ApplicationController < ActionController::Base
 		when true
 			return [true, next_timing]
 		end
-
-
-	end
 end
--- app/helpers/buildings_helper.rb
+++ app/helpers/buildings_helper.rb
@@ -1,5 +1,6 @@
 module BuildingsHelper
 	def rm_free(room)
+		offset = 60*60*5
   		t = Time.now
   		rmevents = Event.where(room_id: room).today.load
   		puts "Room events today : #{rmevents.count}"
@@ -13,12 +14,12 @@ module BuildingsHelper
 			next_timing = Time.new(t.year,t.month,t.day,7)	
 		else
 			for i in 0..(rmevents.count - 1)
-				if t >= (rmevents[i][:start_time].to_time) and t < (rmevents[i][:end_time].to_time)
+				if t >= (rmevents[i][:start_time].to_time+offset) and t < (rmevents[i][:end_time].to_time+offset)
 					available = false
-					next_timing = (rmevents[i][:end_time].to_time)
+					next_timing = (rmevents[i][:end_time].to_time+offset)
 					break
-				elsif t < (rmevents[i][:start_time].to_time) and t > (rmevents[i-1][:end_time].to_time)
-					next_timing = (rmevents[i][:start_time].to_time)
+				elsif t < (rmevents[i][:start_time].to_time+offset) and t > (rmevents[i-1][:end_time].to_time+offset)
+					next_timing = (rmevents[i][:start_time].to_time+offset)
 					break
 				end
 			end
