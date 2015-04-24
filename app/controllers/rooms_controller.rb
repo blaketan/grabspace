94ce1ae4a16e84e12c1048a28e5d11da28133da5
Blake Tan <blaketan@Blakes-MacBook-Pro-2.local>
2015-04-24 14:57:16 -0500

    debug timezones

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,8 @@ class RoomsController < ApplicationController
 
 	def show
 		@room = Room.find(params[:id])
-		@events = @room.events["start_time"].to_time
+		@event = @room.events
+		@events = @event["start_time"]
 		@available = rm_avail(@room.id)
 	end
 end
