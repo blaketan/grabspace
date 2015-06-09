e01b5aa2f2b252f504147442e8f1354e11f16f24
Blake Tan <blaketan@blakes-mbp-2.gateway.sonic.net>
2015-06-09 11:16:58 -0700

    added rooms view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,9 @@
 class RoomsController < ApplicationController
 
-	def show
-		@room = Room.find(params[:id])
-		@events = @room.events.sort
-		@available = [@room.available?,@room.next_timing]
+  def show
+	@room = Room.find(params[:id])
+	@hall = Building.find_by(@room[building_id])
+	@events = @room.events.sort
+	@available = [@room.available?,@room.next_timing]
 	end
 end
